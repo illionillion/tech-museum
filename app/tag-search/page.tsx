3ea85d362ca02caef75583d6e93655bd671a1630
Yuto Terada <lugongdao07@gmail.com>
2024-11-01 22:19:16 +0900

    feat: タグ検索で記事が見つからない場合、テキストを表示するようにした

--- app/tag-search/page.tsx
+++ app/tag-search/page.tsx
@@ -1,6 +1,5 @@
-import { Text, VStack } from "@yamada-ui/react"
+import { Center, Text, VStack } from "@yamada-ui/react"
 import type { Metadata } from "next"
-import { notFound } from "next/navigation"
 import React from "react"
 import { ArticleCard } from "@/components/data-display/article-card"
 import { Layout } from "@/components/layouts"
@@ -25,17 +24,22 @@ const Page = async ({ searchParams }: Props) => {
   const tags = query.split(",")
 
   const articles = (await getArticleList()).filter((article) => article.keyword.some((word) => tags.includes(word)))
-  if (articles.length === 0) {
-    notFound()
-  }
 
   return (
     <Layout>
-      <VStack>
-        {articles.map((article) => (
-          <ArticleCard key={article.slug} article={article} />
-        ))}
-      </VStack>
+      {articles.length > 0
+        ? (
+          <VStack>
+            {articles.map((article) => (
+              <ArticleCard key={article.slug} article={article} />
+            ))}
+          </VStack>
+        )
+        : (
+          <Center flexDir="column" mx="auto" maxW="md" gap="md">
+            <Text>記事が見つかりませんでした</Text>
+          </Center>
+        )}
     </Layout>
   )
 }
