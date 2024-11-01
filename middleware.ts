1f98c265dd65383fad9e9751d2fb8ea4a37695f9
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-01 20:01:37 +0900

    fix: auth.jsの設定変更

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+// import { auth as middleware } from "@/utils/auth/"
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
+
+// export default middleware((req) => {
+
+//   console.dir(req, { depth: null })
+// })
--- utils/auth/middleware.ts
+++
@@ -1,5 +0,0 @@
-export { auth as middleware } from "./auth"
-
-export const config = {
-  //   matcher: "/profile:", // /profile以降のパスにアクセスした場合にmiddlewareが実行される
-}
