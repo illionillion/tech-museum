4ace44db80a8923c5a0eff3ca853b911c101ad27
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-02 13:05:35 +0900

    fix: オプショナル追加

--- app/tag-search/page.tsx
+++ app/tag-search/page.tsx
@@ -19,26 +19,28 @@ const Page = async ({ searchParams }: Props) => {
   const { query } = searchParams
   const queryArray = Array.isArray(query) ? query : [query]
 
-  const tags = queryArray.filter(i => i).flatMap(i => (i as string).split(","))
+  const tags = queryArray
+    .filter((i) => i)
+    .flatMap((i) => (i as string).split(","))
 
-  const articles = (await getArticleList()).filter((article) => article.keyword.some((word) => tags.includes(word)))
+  const articles = (await getArticleList()).filter((article) =>
+    article?.keyword?.some((word) => tags.includes(word)),
+  )
 
   return (
     <Layout>
-      {articles.length > 0
-        ? (
-          <VStack>
-            <Heading>タグ検索: {tags.join(", ")}</Heading>
-            {articles.map((article) => (
-              <ArticleCard key={article.slug} article={article} />
-            ))}
-          </VStack>
-        )
-        : (
-          <Center flexDir="column" mx="auto" maxW="md" gap="md">
-            <Text>記事が見つかりませんでした</Text>
-          </Center>
-        )}
+      {articles.length > 0 ? (
+        <VStack>
+          <Heading>タグ検索: {tags.join(", ")}</Heading>
+          {articles.map((article) => (
+            <ArticleCard key={article.slug} article={article} />
+          ))}
+        </VStack>
+      ) : (
+        <Center flexDir="column" mx="auto" maxW="md" gap="md">
+          <Text>記事が見つかりませんでした</Text>
+        </Center>
+      )}
     </Layout>
   )
 }
