8db9bc85d322e093a1c758da025b2dabfa5b37d6
taku10101 <taku1011ht@gmail.com>
2024-11-04 02:25:47 +0900

    fix import

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -34,7 +34,7 @@ import Link from "next/link"
 import type { FC } from "react"
 import { useRef, useState, memo } from "react"
 import { Search, SearchButton } from "../forms/search"
-import { UserMenu } from "../navigation/user-manu"
+import { UserMenu } from "../navigation/user-menu"
 
 export type HeaderProps = CenterProps
 
